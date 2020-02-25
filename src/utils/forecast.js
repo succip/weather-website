6262ce38de8893b5d0be1ce6bd79cf71be638c7e
Succi <P205934@surrey.ca>
2020-02-25 08:13:54 -0800

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "https://api.darksky.net/forecast/93de3c8e1c1208b99f2dacb8b513cac1/" +
+    latitude +
+    "," +
+    longitude +
+    "?units=si";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unabled to connect to weather service.", undefined);
+    } else if (body.error) {
+      callback("Unable to find weather for location specified.", undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.daily.summary} It is currently ${body.currently.temperature} degrees with a ${body.currently.precipProbability} chance of rain.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
